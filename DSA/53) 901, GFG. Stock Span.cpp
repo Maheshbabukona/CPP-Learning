7075ce865c99c31fea7ad55c7c00e04ceaefb6b6
Maheshbabukona <maheshbabukona9494@gmail.com>
2026-02-11 18:47:24 +0530

    learning 11Feb

--- DSA/53) 901, GFG. Stock Span.cpp
+++ DSA/53) 901, GFG. Stock Span.cpp
@@ -3,6 +3,12 @@
 #include <stack>
 using namespace std;
 
+/*
+Offline - we have array of all prices of stocks.
+traverse, as you go add the index of price into stack. and 
+each time find the prev high thats exisiting in the stack.. remove that are lower. so.. 
+span becomes i-st.top()( contains the index of prev high). 
+*/
 class GFG{
 public:
     vector<int> calculateSpan(vector<int> price){
@@ -12,15 +18,21 @@ public:
         for(int i=0;i<price.size();i++){
             while(st1.size()>0 && price[st1.top()]<=price[i]){
                 st1.pop();
-            }
-            if(st1.size()==0) ans.push_back(i+1);
-            else ans.push_back(i-st1.top());
+            } // after this there can only be 2 cases - stack is empty and stack is not empty
+            if(st1.size()==0) ans.push_back(i+1); // here.. either its first element or all the previous elements are lower
+            else ans.push_back(i-st1.top()); // span is calculated from this index to backwards.. till we are hit with anything higher
             st1.push(i);
         }
         return ans;
     }
 };
 
+/*
+online.. Here you dont have the next prices.. only backwards.. for each price function is called.
+so have a globally declared stack.. store both price and span at each index.. 
+while current price > exisitng prices in the stack.. increase the span.. and 
+update the stack with current price and span.  and return span.
+*/
 class Leetcode{
 public:
     stack<pair<int,int>> st2; // {price, span}
